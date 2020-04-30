c2e6e5c6b5f8ba143466b3f7e5f78328065add45
admin <shy19940918@live.com>
2020-04-30 17:20:43 +0800

    添加vue單文件組件

--- 5-single-component/vue-demo/src/App.vue
+++ 5-single-component/vue-demo/src/App.vue
@@ -1,28 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <todo-list>
+      <todo-item
+        :key="item.title"
+        @delete="handleDelete"
+        v-for="item in list"
+        :title="item.title"
+        :del="item.del"
+      >
+        <template v-slot:pre-icon="{value}">
+          <span>前置插槽 + {{value}}</span>
+        </template>
+        <template v-slot:after-icon="{value}">
+          <span class="red">后置插槽 + {{value}}</span>
+        </template>
+      </todo-item>
+    </todo-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from "./components/TodoItem.vue";
+import TodoList from "./components/TodoList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    TodoItem,
+    TodoList
+  },
+  data() {
+    return {
+      message: "hello world",
+      list: [
+        {
+          title: "课程1",
+          del: false
+        },
+        {
+          title: "课程2",
+          del: true
+        }
+      ],
+      item: {
+        title: "课程1",
+        del: true
+      }
+    };
+  },
+  methods: {
+    handleDelete(val) {
+      console.log("handleDelete", val);
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.red {
+  color: red;
 }
 </style>
---
+++ 5-single-component/vue-demo/src/components/TodoItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <slot name="pre-icon" :value="value"></slot>
+    <span v-if="!del">{{title}}</span>
+    <span v-else style="text-decoration: line-through;">{{title}}</span>
+    <slot name="after-icon" value="默認"></slot>
+    <button v-show="!del" @click="handleClick">删除</button>
+  </li>
+</template>
+<script>
+export default {
+  props: {
+    title: String,
+    del: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data: function() {
+    return {
+      value: Math.random()
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log("点击删除");
+      this.$emit("delete", this.title);
+    }
+  }
+};
+</script>
